ca4fc22399d9edb6d163fb9d5ef111fa95086fc8
everett.bloch <everett.bloch@economicmodeling.com>
2020-03-16 16:42:05 -0700

    Clean up docker networks on docker clean.

--- docker_alias.sh
+++ docker_alias.sh
@@ -1,12 +1,14 @@
 docker(){
-        if [[ "$@" == "clean" ]]; then
-                echo "Stopping all containers..."
-                command docker stop $(docker ps -aq) 2> /dev/null
-                echo "Removing all containers..."
-                command docker rm $(docker ps -aq) 2> /dev/null
-                echo "Removing dangling images..."
-                command docker rmi $(docker images -qf dangling=true) 2> /dev/null
-        else
-                command docker "$@"
-        fi
+    if [[ "$@" == "clean" ]]; then
+        echo "Stopping all containers..."
+        command docker stop $(docker ps -aq) 2> /dev/null
+        echo "Removing all containers..."
+        command docker rm $(docker ps -aq) 2> /dev/null
+        echo "Removing unused networks..."
+        command docker network prune -f 2> /dev/null
+        echo "Removing dangling images..."
+        command docker rmi $(docker images -qf dangling=true) 2> /dev/null
+    else
+        command docker "$@"
+    fi
 }
